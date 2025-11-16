9b10a9aafa2dc8d7d920a2043876f5d6189eb527
Trifan Ciprian <trifan.ciprian09@gmail.com>
2025-11-16 19:57:58 +0200

    Fix messages import path for next-intl

--- app/(site)/[locale]/layout.tsx
+++ app/(site)/[locale]/layout.tsx
@@ -6,11 +6,19 @@ export function generateStaticParams() {
   return [{locale: 'ro'}, {locale: 'en'}];
 }
 
-export default async function LocaleLayout({children, params: {locale}}) {
+export default async function LocaleLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: {locale: string};
+}) {
+  const {locale} = params;
+
   let messages;
   try {
-    messages = (await import(`../../../messages/${locale}.json`)).default;
-  } catch {
+    messages = (await import(`../../messages/${locale}.json`)).default;
+  } catch (error) {
     notFound();
   }
 
